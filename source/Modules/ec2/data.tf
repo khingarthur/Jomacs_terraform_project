e332c160569e8313a695e76630e53792c676271a
Khingarthur <arthurfrederick03@gmail.com>
2023-11-01 11:14:23 +0000

    ec2 files

---
+++ source/Modules/ec2/data.tf
@@ -0,0 +1,15 @@
+# Latest Ubuntu Image For EC2 Instance
+data "aws_ami" "latest_ubuntu_image" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
